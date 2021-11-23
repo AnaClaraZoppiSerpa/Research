37d69d56e2667b3dcbccb698994087fed467c47b
Ana Clara Zoppi Serpa <anaclara.zoppiserpa@gmail.com>
2021-11-23 09:00:04 -0300

    gf2m update

--- code/gf2m.c
+++ code/gf2m.c
@@ -9,7 +9,7 @@
 //Binary representation: 100000000
 //Equivalent poly: x^8
 #define IRREDUCIBLE_POLY SHARK_POLY
-#define DIM 3
+#define DIM 8
 
 /*
 	Other polynomials to add:
@@ -38,6 +38,62 @@ void init_discrete_log() {
 	for (int i = 1; i < TWO_ORDER_POWER - 1; i++) {
 		discrete_log[discrete_log_inverse[i]] = i;
 	}
+}
+
+unsigned int poly_xtime_cost(unsigned int poly) {
+	unsigned int degree_mask = DEGREE_LIMIT_MASK;
+	unsigned int degree = ORDER;
+	while ((poly & degree_mask) == 0) {
+		degree_mask >>= 1;
+		degree--;
+	}
+	return degree;
+}
+
+unsigned int poly_xor_cost(unsigned int poly) {
+	unsigned int mask = 1;
+	unsigned int set_bits = 0;
+	unsigned int current_bit = 0;
+	while (current_bit <= ORDER) {
+		set_bits += ((poly & mask) != 0);
+		mask <<= 1;
+		current_bit++;
+	}
+	return set_bits - 1;
+}
+
+unsigned int matrix_xtime_cost(unsigned int mat [DIM][DIM]) {
+	unsigned int total_cost = 0;
+	for (int row = 0; row < DIM; row++) {
+		unsigned int row_cost = 0;
+		for (int col = 0; col < DIM; col++) {
+			row_cost += poly_xtime_cost(mat[row][col]);
+		}
+		printf("Row %d costs %d xtime\n", row, row_cost);
+		total_cost += row_cost;
+	}
+	printf("The full matrix costs %d xtime\n", total_cost);
+	return total_cost;
+}
+
+unsigned int matrix_xor_cost(unsigned int mat[DIM][DIM]) {
+	unsigned int total_cost = 0;
+	for (int row = 0; row < DIM; row++) {
+		unsigned int row_cost = DIM - 1; //sum elements
+		for (int col = 0; col < DIM; col++) {
+			row_cost += poly_xor_cost(mat[row][col]);
+		}
+		printf("Row %d costs %d xor\n", row, row_cost);
+		total_cost += row_cost;
+	}
+	printf("The full matrix costs %d xor\n", total_cost);
+	return total_cost;
+}
+
+void matrix_total_cost(unsigned int mat[DIM][DIM]) {
+	unsigned int xtime = matrix_xtime_cost(mat);
+	unsigned int xor = matrix_xor_cost(mat);
+	printf("This matrix requires %d xor and %d xtime\n", xor, xtime);
 }
 
 unsigned int add_poly(unsigned int poly1, unsigned int poly2) {
@@ -153,10 +209,53 @@ unsigned int determinant_of_matrix(unsigned int mat[DIM][DIM], unsigned int n) {
     return D;
 }
 
-void test_big_det() {
+/*void test_big_det() {
 	unsigned int a [3][3] = {{2, 3, 4}, {5, 6, 7}, {8, 9, 10}};
 	print_matrix(a, 16);
 	print_poly(determinant_of_matrix(a, 3), 16);
+}*/
+
+/*void test_matrix_cost_square() {
+	unsigned int square_mat [4][4] = {
+		{0x02, 0x01, 0x01, 0x03},
+		{0x03, 0x02, 0x01, 0x01},
+		{0x01, 0x03, 0x02, 0x01},
+		{0x01, 0x01, 0x03, 0x02}
+	};
+	
+	unsigned int square_mat_inv [4][4] = {
+		{0x0e, 0x09, 0x0d, 0x0b},
+		{0x0b, 0x0e, 0x09, 0x0d},
+		{0x0d, 0x0b, 0x0e, 0x09},
+		{0x09, 0x0d, 0x0b, 0x0e}
+	};
+	
+	matrix_total_cost(square_mat_inv);
+}*/
+
+void test_matrix_cost_shark() {
+	unsigned int shark [8][8] = {
+		{0xce, 0x95, 0x57, 0x82, 0x8a, 0x19, 0xb0, 0x01},
+		{0xe7, 0xfe, 0x05, 0xd2, 0x52, 0xc1, 0x88, 0xf1},
+		{0xb9, 0xda, 0x4d, 0xd1, 0x9e, 0x17, 0x83, 0x86},
+		{0xd0, 0x9d, 0x26, 0x2c, 0x5d, 0x9f, 0x6d, 0x75},
+		{0x52, 0xa9, 0x07, 0x6c, 0xb9, 0x8f, 0x70, 0x17},
+		{0x87, 0x28, 0x3a, 0x5a, 0xf4, 0x33, 0x0b, 0x6c},
+		{0x74, 0x51, 0x15, 0xcf, 0x09, 0xa4, 0x62, 0x09},
+		{0x0b, 0x31, 0x7f, 0x86, 0xbe, 0x05, 0x83, 0x34}
+	};
+	
+	unsigned int shark_inv [8][8] = {
+		{0xe7, 0x30, 0x90, 0x85, 0xd0, 0x4b, 0x91, 0x41},
+		{0x53, 0x95, 0x9b, 0xa5, 0x96, 0xbc, 0xa1, 0x68},A AAkkkaaaKKdjskhdoi
+		{0x02, 0x45, 0xf7, 0x65, 0x5c, 0x1f, 0xb6, 0x52},
+		{0xa2, 0xca, 0x22, 0x94, 0x44, 0x63, 0x2a, 0xa2},
+		{0xfc, 0x67, 0x8e, 0x10, 0x29, 0x75, 0x85, 0x71},
+		{0x24, 0x45, 0xa2, 0xcf, 0x2f, 0x22, 0xc1, 0x0e},
+		{0xa1, 0xf1, 0x71, 0x40, 0x91, 0x27, 0x18, 0xa5},
+		{0x56, 0xf4, 0xaf, 0x32, 0xd2, 0xa4, 0xdc, 0x71}
+	};
+	matrix_total_cost(shark_inv);
 }
 
 /*void test_mul() {
@@ -199,7 +298,11 @@ int main() {
 	//test_inv();
 	//test_matrix_mul();
 	//test_det();
-	test_big_det();
+	//test_big_det();
+	//unsigned int p = 0xce;
+	//printf("%d", poly_xtime_cost(p));
+	//printf("%d", poly_xor_cost(p));
+	test_matrix_cost_shark();
 	return 0;
 }
 
