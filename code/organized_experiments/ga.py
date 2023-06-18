770f7710a9530da96225a81ba61d705868a94af3
Ana Clara Zoppi Serpa <anaczs@amazon.com>
2023-06-18 18:36:26 -0300

    experiment ultimate setup

--- code/organized_experiments/ga.py
+++ code/organized_experiments/ga.py
@@ -1,9 +1,10 @@
 import galois
 import math
 import pprint
+import sys
+import datetime
 
 from matrix_evaluation import *
-
 from initial_population_functions import *
 from selection_functions import *
 from pairing_functions import *
@@ -12,6 +13,7 @@ from crossover_functions import *
 from mutation_functions import *
 from replacement_functions import *
 from lookup import *
+from tee import *
 
 BASELINES = {
     8: 296,
@@ -61,80 +63,34 @@ class ChosenFunctions:
         self.mutation_f = mutation_f
         self.replacement_f = replacement_f
 
-class Results:
-    def __init__(self):
-        self.discovered_mds_candidates = []
-        self.already_existing_candidates = []
-        self.discovered_mds_with_same_cost = []
-        self.discovered_mds_with_better_cost = []
-        self.mds_count = 0
-        self.non_mds_count = 0
+class ExperimentReport:
+    def __init__(self, settings, arguments, functions):
+        self.population_tracker = []
+        self.offspring_tracker = []
+        self.settings = settings
+        self.arguments = arguments
+        self.functions = functions
+        self.mds_worse = {}
+        self.mds_same = {}
+        self.mds_better = {}
+        self.u_mds_worse = []
+        self.u_mds_same = []
+        self.u_mds_better = []
+    def extract_unique_for_list(self, mds_list, unique_list):
+        for iteration in mds_list:
+            for candidate in mds_list[iteration]:
+                if not already_in_list(unique_list, candidate):
+                    unique_list.append(candidate)
+    def extract_unique(self):
+        self.extract_unique_for_list(self.mds_worse, self.u_mds_worse)
+        self.extract_unique_for_list(self.mds_same, self.u_mds_same)
+        self.extract_unique_for_list(self.mds_better, self.u_mds_better)
 
-    def mds_discovery(self, candidate):
-        self.discovered_mds_candidates.append(candidate)
-
-    def repeated(self, candidate):
-        self.already_existing_candidates.append(candidate)
-
-    def mds_same_cost_discovery(self, candidate):
-        self.discovered_mds_with_same_cost.append(candidate)
-
-    def mds_better_cost_discovery(self, candidate):
-        self.discovered_mds_with_better_cost.append(candidate)
-
-    def count_mds(self):
-        self.mds_count += 1
-
-    def count_non_mds(self):
-        self.non_mds_count += 1
-
-    def print_compact(self):
-        print("MDS count X non-mds count:", self.mds_count, "X", self.non_mds_count)
-        print("MDS worse discovered:", len(self.discovered_mds_candidates))
-        print("Repeated:", len(self.already_existing_candidates))
-        print("MDS same:", len(self.discovered_mds_with_same_cost))
-        print("MDS better:", len(self.discovered_mds_with_better_cost))
-
-    def print_full(self):
-        print("MDS count X non-mds count:", self.mds_count, "X", self.non_mds_count)
-        print("MDS worse discovered:", len(self.discovered_mds_candidates))
-        print("Repeated:", len(self.already_existing_candidates))
-        print("MDS same:", len(self.discovered_mds_with_same_cost))
-        print("MDS better:", len(self.discovered_mds_with_better_cost))
-
-        # Imprimir quais foram as matrizes repetidas, se existirem
-        # Imprimir as MDS piores
-        # Imprimir as MDS melhores
-        # Imprimir as MDS iguais
-
-        print("The repeated matrices found were these:")
-        for candidate in self.already_existing_candidates:
-            print(candidate.dataset_id)
-
-        print("The MDS matrices found were these:")
-        for candidate in self.discovered_mds_candidates:
-            print("cost =", candidate.cost, "baseline diff =", candidate.baseline_diff)
-            print("dim", candidate.dim)
-            print("xor", candidate.xor)
-            print("xtime", candidate.xtime)
-            print(candidate.matrix)
-
-        print("The MDS matrices with same cost as baseline found were these:")
-        for candidate in self.discovered_mds_with_same_cost:
-            print("cost =", candidate.cost, "baseline diff =", candidate.baseline_diff)
-            print("dim", candidate.dim)
-            print("xor", candidate.xor)
-            print("xtime", candidate.xtime)
-            print(candidate.matrix)
-
-        print("The MDS matrices with better cost found were these:")
-        for candidate in self.discovered_mds_with_better_cost:
-            print("cost =", candidate.cost, "baseline diff =", candidate.baseline_diff)
-            print("dim", candidate.dim)
-            print("xor", candidate.xor)
-            print("xtime", candidate.xtime)
-            print(candidate.matrix)
-
+def already_in_list(list, new_element):
+    for list_element in list:
+        if matrix_equals(list_element["matrix"], new_element["matrix"]):
+            return True
+    return False
 
 class Candidate:
     def __init__(self, matrix, mds, invertible, xor, xtime, inverse, dim):
@@ -191,41 +147,83 @@ def to_candidates(list_of_matrices, E):
         c = get_candidate_data(matrix, E.field, E.order)
         candidates.append(c)
     return candidates
+def to_dict_array(candidates):
+    array = []
+    for c in candidates:
+        array.append(c.__dict__)
+    return array
 
+def add_to_dict(dict, i, c):
+    if i not in dict:
+        dict[i] = []
+    if not already_in_list(dict[i], c):
+        dict[i].append(c)
 
-def log(candidates, log_string):
-    print(log_string)
-    print("len =", len(candidates))
-    for (i, candidate) in enumerate(candidates):
-        print("candidate", i)
-        print("fitness", candidate.fitness())
-        print("ranking", candidate.ranking)
-        print("diff", candidate.baseline_diff)
-        print("exists?", candidate.already_exists)
-        print("id", candidate.dataset_id)
-        print("mds?", candidate.mds)
-        print(candidate.matrix)
+def add_to_correct_dict(R, i, c):
+    if c["ranking"] == "WORSE":
+        add_to_dict(R.mds_worse, i, c)
+    elif c["ranking"] == "SAME":
+        add_to_dict(R.mds_same, i, c)
+    elif c["ranking"] == "BETTER":
+        add_to_dict(R.mds_better, i, c)
+def store_relevant_info(R, i):
+    population = R.population_tracker[i]
+    offspring = R.offspring_tracker[i]
 
-def result_storage(candidates, R):
-    for (i, candidate) in enumerate(candidates):
-        if candidate.mds:
-           R.count_mds()
-        else:
-            R.count_non_mds()
+    for c in population:
+        if c["mds"]:
+            print("(population) MDS candidate present at iteration", i, "ranking=", c["ranking"])
+            add_to_correct_dict(R, i, c)
+            print(c["matrix"])
+        if c["ranking"] == "SAME" or c["ranking"] == "BETTER":
+            print("DISCOVERY!!!")
 
-        if candidate.already_exists:
-            R.repeated(candidate)
-        elif candidate.mds:
-            if candidate.ranking == "SAME":
-                R.mds_same_cost_discovery(candidate)
-            elif candidate.ranking == "BETTER":
-                R.mds_better_cost_discovery(candidate)
-            else:
-                R.mds_discovery(candidate)
+    for c in offspring:
+        if c["mds"]:
+            print("(offspring) MDS candidate present at iteration", i, "ranking=", c["ranking"])
+            add_to_correct_dict(R, i, c)
+            print(c["matrix"])
+        if c["ranking"] == "SAME" or c["ranking"] == "BETTER":
+            print("DISCOVERY!!!")
 
-def evolve(E, A, F):
-    R_population = Results()
-    R_offspring = Results()
+def print_summary(R):
+    print("MDS better")
+
+    for m in R.u_mds_better:
+        print("xor", m["xor"], "xtime", m["xtime"], "baseline diff", m["baseline_diff"])
+        print(m["matrix"])
+
+    print("MDS same")
+
+    for m in R.u_mds_same:
+        print("xor", m["xor"], "xtime", m["xtime"], "baseline diff", m["baseline_diff"])
+        print(m["matrix"])
+
+    print("MDS worse")
+
+    for m in R.u_mds_worse:
+        print("xor", m["xor"], "xtime", m["xtime"], "baseline diff", m["baseline_diff"])
+        print(m["matrix"])
+
+def evolve(E, A, F, fun):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+    command_line = ""
+    if len(sys.argv) > 1:
+        command_line = sys.argv[1]
+    log_file_path = "EXP/log-" + command_line + "-" + fun + "-" + timestamp
+    log_file = open(log_file_path, 'w')
+    full_data_path = "EXP/full-" + command_line + "-" + fun + "-" + timestamp
+    full_data_file = open(full_data_path, 'w')
+
+    sys.stdout = Tee(sys.stdout, log_file)
+
+    R = ExperimentReport(E.__dict__, A.__dict__, F.__dict__)
+
+    print("EXPERIMENT IDENTIFICATION:", command_line, fun, timestamp)
+    print("EXPERIMENT PARAMETERS")
+    print(E.__dict__)
+    print(A.__dict__)
+    print(F.__dict__)
 
     population = to_candidates(
         F.initialization_f(A.initial_population_size, E.integer_upper_limit, E.dimension, E.dimension, A), E)
@@ -233,36 +231,32 @@ def evolve(E, A, F):
     iteration_count = 0
 
     while iteration_count < A.max_iterations:
-        print("ITERATION", iteration_count+1, "/", A.max_iterations)
+        print(f"ITERATION {iteration_count+1} / {A.max_iterations}")
 
-        result_storage(population, R_population)
-        log(population, "starting population, iteration:" + str(iteration_count))
+        R.population_tracker.append(to_dict_array(population))
 
         selected_parents = F.selection_f(population, A.selection_num_parents)
         parent_pairs = F.pairing_f(selected_parents, A)
         offspring = to_candidates(reproduction(parent_pairs, A.mutation_prob, F.crossover_f, F.mutation_f, A), E)
 
-        result_storage(offspring, R_offspring)
-        log(offspring, "offspring, iteration:" + str(iteration_count))
+        R.offspring_tracker.append(to_dict_array(offspring))
+
+        store_relevant_info(R, iteration_count)
 
         population = F.replacement_f(population, offspring, A)
 
         iteration_count += 1
 
-    print("Population analysis:")
-    R_population.print_full()
-    print("Offspring analysis:")
-    R_offspring.print_full()
+    R.extract_unique()
 
-def test():
-    E = ExperimentSettings(4, 3, GF2_4)
-    A = Arguments(E.integer_upper_limit, 6, 6, 6, 3, 10, 400, 8, 0.1)
-    F = ChosenFunctions(random_initialization,
-                        selection1,
-                        pairing1,
-                        crossover1,
-                        mutation1,
-                        replacement1)
-    evolve(E, A, F)
+    print("SUMMARY")
+    print_summary(R)
 
-test()
+    sys.stdout = Tee(full_data_file)
+
+    print("EXPERIMENT FULL DATA")
+    pprint.pprint(R.__dict__)
+
+    sys.stdout = sys.__stdout__
+    log_file.close()
+    full_data_file.close()
