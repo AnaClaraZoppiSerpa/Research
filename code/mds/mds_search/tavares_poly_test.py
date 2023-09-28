4c3e48052b164b61241735063b950fe6a321963d
Ana Clara Zoppi Serpa <anaczs@amazon.com>
2023-09-28 17:03:39 -0300

    backup 28 sept 2023

--- code/mds/mds_search/tavares_poly_test.py
+++ code/mds/mds_search/tavares_poly_test.py
@@ -1,5 +1,8 @@
 from metrics_and_flags import *
 
+correct_poly = "x^8 + x^4 + x^3 + x^2 + 1" # (GF(2^8), poly)
+wrong_poly = "x^8 + x^4 + x^3 + x + 1" # ()
+
 tavares_mat = [
     [0x93, 0x13, 0x57, 0xda, 0x58, 0x47, 0x0c, 0x1f],
     [0x13, 0x93, 0xda, 0x57, 0x47, 0x58, 0x1f, 0x0c],
@@ -12,7 +15,7 @@ tavares_mat = [
 ]
 
 GF2 = galois.GF(2)
-poly = galois.Poly.Str("x^8 + x^4 + x^3 + x + 1", field=GF2)
+poly = galois.Poly.Str(correct_poly, field=GF2)
 field = galois.GF(2**8, irreducible_poly=poly)
 
 int_to_gf_mat(tavares_mat, field)
